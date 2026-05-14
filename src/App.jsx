4e05da343d9ab3b6c17585c53eb56d2744537cdc
Tui <m.satit@gmail.com>
2026-05-14 17:03:08 +0700

    update cart

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,16 @@ import { useState } from "react";
 
 function App() {
    const [cart, setCart] = useState([]);
+   
    const [customerName, setCustomerName] = useState("");
-
+   const [orders, setOrders] = useState([]);
   const menu = [
     {
       id: 1,
       name: "Americano",
       price: 50,
       image: americano,
+      type: "Hot",
     },
 
      {
@@ -24,6 +26,7 @@ function App() {
       name: "Latte",
       price: 60,
       image: latte,
+      type: "Hot",
     },
 
      {
@@ -31,34 +34,35 @@ function App() {
       name: "Cappuccino",
       price: 70,
       image: cappuccino,
+      type: "Hot",
     },
   ];
 
-  const addToCart = (item) => {
+  const addToCart = (newItem) => {
+    const existingItem = cart.find(
+      (item) => 
+      item.id === newItem.id &&
+      item.type === newItem.type &&
+      item.sweetness === newItem.sweetness
+);
+  
+
+  if (existingItem) {
+    const updatedCart = cart.map((item) =>
+    item.id === newItem.id &&
+    item.type === newItem.type &&
+    item.sweetness === newItem.sweetness
+    ? { ...item, quantity: item.quantity + 1}
+    : item
+  );
+
+     setCart(updatedCart);
+      } else {
+        setCart([...cart, { ...newItem, quantity: 1}]);
+      }
+
     
-    const existingItem = cart.find (
-      (cartItem) => cartItem.id === item.id
-    );
-
-    if (existingItem) {
-
-      const updateCart = cart.map((cartItem) => 
-       cartItem.id === item.id 
-    ? {
-      ...cartItem,
-      quantity: cartItem.quantity + 1,
-    }
-      : cartItem
-    );
-
-      setCart(updateCart);
-
-} else {
-    setCart([
-      ...cart,
-      { ...item, quantity: 1,},
-    ]);
-}
+ 
   };
 
   const increaseQuantity = (id) => {
@@ -87,6 +91,36 @@ function App() {
       setCart(updatedCart);
       };
 
+  const clearCart = () => {
+    setCart([]);
+  };
+
+  const submitOrder = () => {
+     if (customerName.trim() === "") {
+      alert("Please enter customer name")
+      return;
+    }
+    const newOrder = {
+      id: Date.now(),
+      customerName: customerName,
+      items: cart,
+      total: totalPrice,
+    };
+
+    setOrders([...orders, newOrder]);
+
+    setCart([]);
+
+     setCustomerName("");
+
+    alert("Order submitted ☕");
+
+    if (cart.length === 0) {
+      alert("Cart is empty");
+      return;
+    }
+  };
+
   const totalPrice = cart.reduce((sum, item) => {
     return sum + item.price * item.quantity;
   }, 0);
@@ -94,6 +128,8 @@ function App() {
   const totalItems = cart.reduce((sum, item) => {
     return sum + item.quantity;
   }, 0);
+
+  
 
   return (
      <div>
@@ -103,9 +139,12 @@ function App() {
        🛒 {totalItems}
 
     </div>
+   
+
+   
 
     <div className="customer-box">
-
+        
        <input
           type="text"
           placeholder="Enter your name"
@@ -116,45 +155,80 @@ function App() {
          <h3>Hello, {customerName}</h3>
     </div>
     <div className="cart-box">
-      <h2>Cart:{cart.length}</h2>
 
-      {cart.map((item, index) => (
-         <div key={item.id} className="cart-item">
+    <h2>Cart: {cart.length === 0 && (
+      <p>Your cart is empty ☕ </p>
+    )}</h2>
+
+    {cart.map((item, index) => (
+      <div key={index} className="cart-item">
+        <p>{item.type}</p>
+          <p>{item.sweetness}</p>
 
           <p>
             {item.name} x {item.quantity}
           </p>
 
-          <p>
-            {item.price * item.quantity} THB
+           <p>
+            {item.price} x {item.quantity}
           </p>
 
-          <div className="qty-button">
+          <div>
             <button onClick={() => decreaseQuantity(item.id)}>
               -
-              </button>
+            </button>
 
             <button onClick={() => increaseQuantity(item.id)}>
               +
-              </button>
+            </button>
           </div>
           </div>
-     ))}
+     
+    ) )}
+
+    <h2>Order History</h2>
+
+    {orders.length === 0 ? (
+      <p>No orders yet </p>
+    ) : (
+      orders.map((order) => (
+        <div key={order.id} className="order-card">
+          <h3>Customer: {order.customerName}</h3>
+          <p>Total: {order.total} Baht</p>
+
+          {order.items.map((item) => (
+            <p key={item.id}>
+              {item.name} x {item.quantity}
+            </p>
+          ))}
+        </div>
+      ))
+    )}
+
+      <h2>Total: {totalPrice} THB</h2>
+
+      <button onClick={clearCart}>  🗑  Clear Cart</button>
+
+      <button onClick={submitOrder}> Submit Order </button>
+  </div>
+
+    
+        
 
       <hr />
 
       {menu.map((item) => (
         <MenuCard 
           key={item.id}
+          id={item.id}
           name={item.name}
           price={item.price}
           image={item.image}
-          addToCart={() => addToCart(item)}
+          addToCart={addToCart}
         />
       ))}
      </div>
-    </div>
-  
+   
   );
 }
 
--- src/MenuCard.jsx
+++ src/MenuCard.jsx
@@ -1,4 +1,9 @@
+import { useState} from "react";
+
 function MenuCard(props) {
+  const [drinkType, setDrinkType] = useState("Cold");
+  const [sweetness, setSweetness] = useState("Normal");
+
   return (
     <div className="card">
      
@@ -11,9 +16,49 @@ function MenuCard(props) {
 
        <h2>{props.name}</h2>
 
+       <button
+        className={drinkType === "Hot" ? "active-hot" : ""} 
+        onClick={() => setDrinkType("Hot")}> ☕ Hot</button>
+
+        <button
+         className={drinkType === "Cold" ? "active-cold" : ""}
+         onClick={() => setDrinkType("Cold")}> 🧊 Cold</button>
+
+         <div className="sweetness-options">  
+          <button
+          className={sweetness === "Normal" ? "active-sweet" : ""}
+          onClick={() => setSweetness("Normal")}>
+            Normal
+          </button>   
+
+           <button
+            className={sweetness === "Less Sweet" ? "active-sweet" : ""}
+            onClick={() => setSweetness("Less Sweet")}>
+            Less Sweet
+          </button>
+
+           <button
+            className={sweetness === "No Sugar" ? "active-sweet" : ""}
+            onClick={() => setSweetness("No Sugar")}>
+            No Sugar
+          </button>
+ </div>
+
+        <p>Selected: {drinkType}</p>
+        <p>Sweetness: {sweetness}</p>
+
        <p>{props.price} THB</p>
 
-       <button onClick={props.addToCart}>Add to Cart</button>
+       <button onClick={() =>
+        props.addToCart ({
+          id: props.id,
+          name: props.name,
+          price: props.price,
+          image: props.image,
+          type: drinkType,
+          sweetness: sweetness,
+        })
+        }>Add to Cart</button>
     </div>
   );
 }
